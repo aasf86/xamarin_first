e414695db109728a0a7d610c3d723cb0ee6d4f01
Anderson Fernandes <aasf_86@hotmail.com>
2017-07-28 13:50:00 -0300

    commit firt

---
+++ LostingMoney/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LostingMoney
+{
+    [Activity(Label = "LostingMoney", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
