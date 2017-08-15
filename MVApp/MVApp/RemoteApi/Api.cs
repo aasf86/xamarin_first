fbc651e6199b71052637caf15e85ac3d49622d2b
Anderson Fernandes <aasf86@gmail.com>
2017-08-15 16:02:52 -0300

    activityindicator

---
+++ MVApp/MVApp/RemoteApi/Api.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MVApp.RemoteApi
+{
+    public static class Api
+    {
+        public static async Task<List<Model.User>> GetUsersAsync(string search)
+        {
+            using (var hC = new HttpClient())
+            {
+                hC.BaseAddress = new Uri("http://jsonplaceholder.typicode.com");
+                var json = await hC.GetStringAsync("/users");
+                var users = JsonConvert.DeserializeObject<List<Model.User>>(json);
+                return users;
+            }            
+        }
+    }
+}
--- MVApp/MVApp/ViewModels/UserVM.cs
+++ MVApp/MVApp/ViewModels/UserVM.cs
@@ -4,22 +4,40 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 using System.Linq;
+using System.ComponentModel;
+using Xamarin.Forms;
 
 namespace MVApp.ViewModels
 {
-    public class UserVM
+    public class UserVM : INotifyPropertyChanged
     {
         public ICommand CarregarCommand { get; set; }
 
-        public ObservableCollection<Model.User> Users { get; set; }
+        private ObservableCollection<Model.User> _users;
+        public ObservableCollection<Model.User> Users { get { return _users; } set { _users = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Users")); } }
+
+        private bool _running;
+        public bool Running { get { return _running; } set { _running = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Running")); } }
+
+        private string _log;
+        public string Log { get { return _log; } set { _log = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Log")); } }
 
         public UserVM()
         {
             Users = new ObservableCollection<Model.User>();
 
-
-            CarregarCommand = new Xamarin.Forms.Command(() => 
+            CarregarCommand = new Xamarin.Forms.Command(async () => 
             {
+                Running = true;
+                Log = "Rodando...";
+
+                var result = await RemoteApi.Api.GetUsersAsync("");
+
+                Users = new ObservableCollection<Model.User>(result);
+
+                Running = false;
+                Log = "#Concluído#";
+                /*
                 Users.Add(new Model.User
                 {
                     Id=Users.Count(),
@@ -31,7 +49,10 @@ namespace MVApp.ViewModels
                     Id = Users.Count(),
                     Name = $"Edneia {Users.Count()} - {Guid.NewGuid().ToString().Replace("-","")}"
                 });
+                */
             });
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
